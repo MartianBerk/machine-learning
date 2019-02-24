10073f4172304b0615c5a7d26c58037e21b91396
martianberk <baker.bsc@gmail.com>
2019-02-24 22:23:23 +0000

    adding linear regression exercise

--- tutorial/chapter3.py
+++ tutorial/chapter3.py
@@ -1,6 +1,7 @@
 """
-CHapter 3 - Scikit-learn.
+Chapter 3 - Scikit-learn.
 Perceptron
+Linear Regression
 """
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,6 +13,8 @@ from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
+from lib.classifiers.factory import ClassifierFactory
 
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
@@ -49,6 +52,47 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                     s=100, label='test set')
 
 
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def illustrate_single_sample():
+    def cost_1(z):
+        return - np.log(sigmoid(z))
+
+    def cost_0(z):
+        return - np.log(1 - sigmoid(z))
+
+    z = np.arange(-10, 10, 0.1)
+    phi_z = sigmoid(z)
+    c1 = [cost_1(x) for x in z]
+    plt.plot(phi_z, c1, label="J(x) if y=1")
+    c0 = [cost_0(x) for x in z]
+    plt.plot(phi_z, c0, linestyle="--", label="J(w) if y=0")
+    plt.ylim(0.0, 5.1)
+    plt.xlim([0, 1])
+    plt.xlabel("$\phi$(z)")
+    plt.ylabel("J(w)")
+    plt.legend(loc="best")
+    plt.show()
+
+
+def plot_sigmoid():
+    z = np.arange(-7, 7, 0.1)
+    phi_z = sigmoid(z)
+    plt.plot(z, phi_z)
+    plt.axvline(0.0, color="k")
+    plt.ylim(-0.1, 1.1)
+    plt.xlabel("z")
+    plt.ylabel("$\phi (z)$")
+
+    # y axis ticks and gridline
+    plt.yticks([0.0, 0.5, 1.0])
+    ax = plt.gca()
+    ax.yaxis.grid(True)
+    plt.show()
+
+
 if __name__ == "__main__":
     iris = datasets.load_iris()
     X = iris.data[:, [2, 3]]
@@ -66,22 +110,37 @@ if __name__ == "__main__":
     # print("Labels counts in y_test: {}".format(np.bincount(y_test)))
 
     # feature scaling with StandardScaler, using same params for train and test data
-    sc = StandardScaler()
-    sc.fit(X_train)
-    X_train_std = sc.transform(X_train)
-    X_test_std = sc.transform(X_test)
+    # sc = StandardScaler()
+    # sc.fit(X_train)
+    # X_train_std = sc.transform(X_train)
+    # X_test_std = sc.transform(X_test)
 
-    ppn = Perceptron(n_iter=40, eta0=0.1, random_state=1)
-    ppn.fit(X_train_std, y_train)
+    # ppn = Perceptron(n_iter=40, eta0=0.1, random_state=1)
+    # ppn.fit(X_train_std, y_train)
 
-    y_pred = ppn.predict(X_test_std)
-    print('Misclassified samples: %d' % (y_test != y_pred).sum())
-    print("Accuracy: %.2f" % accuracy_score(y_test, y_pred))
+    # y_pred = ppn.predict(X_test_std)
+    # print('Misclassified samples: %d' % (y_test != y_pred).sum())
+    # print("Accuracy: %.2f" % accuracy_score(y_test, y_pred))
 
-    X_combined_std = np.vstack((X_train_std, X_test_std))
-    y_combined = np.hstack((y_train, y_test))
-    plot_decision_regions(X_combined_std, y_combined, ppn, test_idx=range(105, 150))
+    # X_combined_std = np.vstack((X_train_std, X_test_std))
+    # y_combined = np.hstack((y_train, y_test))
+    # plot_decision_regions(X_combined_std, y_combined, ppn, test_idx=range(105, 150))
+    # plt.xlabel("petal length [standardized]")
+    # plt.ylabel("petal width [standardized]")
+    # plt.legend(loc="upper left")
+    # plt.show()
+
+    # plot_sigmoid()
+    # illustrate_single_sample()
+
+    # Demo linear regression, using classes 0 and 1 as only binary classification possible
+    X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
+    y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+    lrgd = ClassifierFactory.get("linearregressiongd", eta=0.05, n_iter=1000, random_state=1)
+    lrgd.fit(X_train_01_subset, y_train_01_subset)
+    plot_decision_regions(X_train_01_subset, y_train_01_subset, classifier=lrgd)
     plt.xlabel("petal length [standardized]")
     plt.ylabel("petal width [standardized]")
     plt.legend(loc="upper left")
     plt.show()
+
--- tutorial/lib/classifiers/factory.py
+++ tutorial/lib/classifiers/factory.py
@@ -1,13 +1,16 @@
 from .adaline import AdalineGD, AdalineSGD
+from .linearregression import LinearRegressionGD
 from .perceptron import Perceptron
 
 
 class ClassifierFactory:
     @staticmethod
-    def get(rule, eta=0.1, n_iter=10, random_state=1, **kwargs):
-        if rule == 'perceptron':
+    def get(classifier, eta=0.1, n_iter=10, random_state=1, **kwargs):
+        if classifier == 'perceptron':
             return Perceptron(eta=eta, n_iter=n_iter, random_state=random_state)
-        elif rule == 'adalinegd':
+        elif classifier == 'adalinegd':
             return AdalineGD(eta=eta, n_iter=n_iter, random_state=random_state)
-        elif rule == 'adalinesgd':
+        elif classifier == 'adalinesgd':
             return AdalineSGD(eta=eta, n_iter=n_iter, random_state=random_state)
+        elif classifier == "linearregressiongd":
+            return LinearRegressionGD(eta=eta, n_iter=n_iter, random_state=random_state)
